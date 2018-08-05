ce74089abe99d7213c5c8d06dec3a270c49bce49
Roman <rozario91208@gmail.com>
2018-08-05 21:44:16 +0300

    first component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hi');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends React.Component {
+
+    render(){
+        return (
+            <h1>sdgdsgAAAAA</h1>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
