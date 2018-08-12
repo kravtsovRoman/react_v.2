b50853883bec9865e6e1fdb4e48fe261eee5fc9b
Roman <rozario91208@gmail.com>
2018-08-12 20:30:59 +0300

    router

--- src/index.js
+++ src/index.js
@@ -1,35 +1,32 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import NewComponent from './new';
 
 
 class App extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-
-        this.state = {
-            text: ''
-        };
-    }
-
-    handleChange(e) {
-        console.log(e.target.value);
-        const text = e.target.value;
-        this.setState({text: text});
     }
     
     render() {
         return (
             <div>
-                <h1 style={{color: 'red'}}> {this.state.text} </h1>
-                <input type="text" value={ this.state.text } onChange={this.handleChange.bind(this)}/>
+                <h1>App Component</h1>
             </div>
         );
     }
 }
 
 ReactDOM.render(
-    <App />,
+    <BrowserRouter>
+        <Switch>
+            <Route exact  path={'/'} component={ App } />
+            <Route path={'/new'} component={ NewComponent } />
+        </Switch>
+    </BrowserRouter>,
     document.getElementById('app')
 );
 
---
+++ src/new.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+export default class NewComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h2>New Component</h2>
+                <p>test</p>
+            </div>
+        );
+    }
+}
