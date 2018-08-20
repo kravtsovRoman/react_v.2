49cb7586972417177c64bf75ca97675585244cd6
Roman <rozario91208@gmail.com>
2018-08-20 18:52:09 +0300

    util devtools settings

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducers from './reducers';
+import { DevTools } from './utils/index';
 
 function _applyMiddleware() {
     const middleWare = [
@@ -11,7 +12,8 @@ function _applyMiddleware() {
 
 export default function configureStore(initialState) {
     const store = compose(
-        _applyMiddleware()
+        _applyMiddleware(),
+        DevTools.instrument()
     )(createStore)(rootReducers, initialState);
 
     return store;
