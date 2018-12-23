58115851148dc0f6171e2d4ccd1268d3fe9522f9
Roman <rozario91208@gmail.com>
2018-12-23 18:14:04 +0200

    inline style

--- src/App.js
+++ src/App.js
@@ -3,26 +3,19 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const divStyle = {
+      'border': '3px solid #ccc',
+      'color': 'red'
+    };
 
     return (
-      <div className="App">
-        <h1>
+      <div className="App" style={divStyle} >
+        <h1 style={{fontSize: '60px'}}>
           Hello
         </h1>
+        <h3 style={{color: 'blue'}}>Re Re</h3>
       </div>
     );
-
-    // return React.createElement(
-    //   'div',
-    //   {
-    //     className: 'App'
-    //   },
-    //   React.createElement(
-    //     'h1',
-    //     null,
-    //     'Hello world!'
-    //   )
-    // )
   }
 }
 
