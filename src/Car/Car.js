d8d503d3d1f636876d81cc31eefa224a7f93fe3c
Roman <rozario91208@gmail.com>
2019-01-05 12:36:26 +0200

    style & Radium library

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React from 'react'
+import Radium from 'radium'
+import './car.css';
 
-const car = (props) => (
-  <div style={{
+const car = (props) => {
+  const inputClasses = ['input'];
+
+  if (props.name !== '') {
+    inputClasses.push('green');
+  } else {
+    inputClasses.push('red');
+  }
+
+  if (props.name.length > 4) {
+    inputClasses.push('bold');
+  }
+
+  const style = {
     border: '2px solid #ccc',
-    margin: '10px',
-    display: 'inline-block',
-    padding: '10px'
-  }}>
-    <h3>Car name: {props.name}</h3>
-    <p>Year: {props.year}</p>
-    <input type="text" onChange={props.onChangeName} value={props.name}/>
-    <button onClick={props.onDelete}>Delete</button>
-  </div>
-);
+    boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .2)',
+    ':hover': {
+      border: '2px solid #aaa',
+      boxShadow: '0 4px 15px 0 rgba(0, 0, 0, .4)',
+      cursor: 'pointer'
+    }
+  }
 
-export default car;
+  return (
+    <div className="car" style={style}>
+      <h3>Car name: {props.name}</h3>
+      <p>Year: {props.year}</p>
+      <input
+        type="text"
+        onChange={props.onChangeName}
+        value={props.name}
+        className={inputClasses.join(' ')}
+      />
+      <button onClick={props.onDelete}>Delete</button>
+    </div>
+  )
+};
+
+export default Radium(car);
