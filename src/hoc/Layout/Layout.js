f0c584693ae6211080a9c9ae59724ea4bbd04b25
Roman <rozario91208@gmail.com>
2019-01-26 19:11:20 +0200

    create main component

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -5,7 +5,6 @@ class Layout extends Component{
     render() {
         return(
             <div className={classes.Layout}>
-
                 <main>
                     {this.props.children}
                 </main>
