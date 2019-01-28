7d6ae3fb901f479dc48fda330e0ac1054332841a
Roman <rozario91208@gmail.com>
2019-01-28 21:46:15 +0200

    Link & style for list

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
+import classes from './QuizList.css';
+import {NavLink} from 'react-router-dom';
 
 export default class QuizList extends Component {
+
+    renderQuizes() {
+        return [1, 2, 3].map((quiz, index) => {
+            return(
+                <li key={index}>
+                    <NavLink to={'/quiz/' + quiz}>
+                        Test {quiz}
+                    </NavLink>
+                </li>
+            )
+        })
+    }
+
+
     render() {
         return(
-            <div>
-                <h1>QuizList</h1>
+            <div className={classes.QuizList}>
+                <div>
+                    <h1>Список тестов</h1>
+
+                    <ul>
+                        {this.renderQuizes()}
+                    </ul>
+                </div>
             </div>
         )
     }
