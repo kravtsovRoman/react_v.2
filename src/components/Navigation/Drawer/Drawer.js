f51316c8c781f50ad25d9b280d82efb16bf4adda
Roman <rozario91208@gmail.com>
2019-01-28 21:28:47 +0200

    Routing Links

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,16 +1,43 @@
 import React, {Component} from 'react';
 import classes from './Drawer.css';
 import Backdrop from "../../UI/Backdrop/Backdrop";
+import {NavLink} from 'react-router-dom';
 
-const links = [ 1,2,3];
+const links = [
+    {
+        to: '/',
+        label: 'Список',
+        exact: true
+    },
+    {
+        to: '/auth',
+        label: 'Авторизация',
+        exact: false
+    },
+    {
+        to: '/quiz-creator',
+        label: 'Содзать тест',
+        exact: false
+    }
+];
+
+
 
 class Drawer extends Component {
+
 
     renderLinks() {
         return(links.map((link, index) => {
             return (
                 <li key={index}>
-                    <a>Link {link}</a>
+                    <NavLink
+                        to={link.to}
+                        exact={link.exact}
+                        activeClassName={classes.active}
+                        onClick={this.props.onClose}
+                    >
+                        {link.label}
+                    </NavLink>
                 </li>
             )
         }))
